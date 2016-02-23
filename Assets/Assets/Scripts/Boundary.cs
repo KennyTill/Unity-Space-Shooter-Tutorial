10011bea3e94fb89ea130c2ae44abfa0b93496b9
Semiceri <semisnowy@gmail.com>
2016-02-23 00:58:36 -0800

    Creating repo at the end of lesson 7, contains most of the assets from the unity store

---
+++ Assets/Assets/Scripts/Boundary.cs
@@ -0,0 +1,11 @@
+﻿
+/*Pulling this class out of the player class. Single class per file is better convention*/
+[System.Serializable]
+public class Boundary {
+
+	//One line for each declaration. It makes it more readable!
+	public float xMin;
+	public float xMax;
+	public float zMin;
+	public float zMax;
+}
---
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+	public float tilt;
+
+	public Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire = 0.0f;
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+	}
+
+
+	void Update(){
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			//will need to clean these up after they go off the screen
+			Object.Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal*speed, 0.0f, moveVertical*speed);
+		rb.velocity = movement;
+		rb.position = new Vector3 (
+			Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+		);
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+}
