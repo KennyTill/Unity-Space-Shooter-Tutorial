1f61d5df7ec1efc930203677a9b3cfc7e48b2d0f
Semiceri <semisnowy@gmail.com>
2016-02-24 19:56:58 -0800

    astroid prefab and assigning vfx to destruction (lesson 10)

--- Assets/Assets/Scripts/DestroyByContact.cs
+++ Assets/Assets/Scripts/DestroyByContact.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
 
+	public GameObject explosion;
+	public GameObject playerExplosion;
 	void OnTriggerEnter(Collider other){
 		if (other.tag == "Boundary") {
 			return;
 		}
-		Debug.Log ("Tag: " + other.tag + ", Object Name: " + other.name);
+		Object.Instantiate(explosion, transform.position, transform.rotation);
 		Destroy (other.gameObject);
 		Destroy (gameObject);
+
+		if (other.tag == "Player") {
+			Object.Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+		}
+
 	}
 }
