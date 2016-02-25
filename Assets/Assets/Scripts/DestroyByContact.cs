79d60d6118b827880123a87918114a7abe3b49d4
Semiceri <semisnowy@gmail.com>
2016-02-24 19:45:17 -0800

    adding in hazards (Lesson 9)

---
+++ Assets/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Boundary") {
+			return;
+		}
+		Debug.Log ("Tag: " + other.tag + ", Object Name: " + other.name);
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
